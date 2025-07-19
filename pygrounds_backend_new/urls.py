34e590012e4f46e257c86ea41558f5344ab65bb4
donnabell-s <donnasembrano03@gmail.com>
2025-07-19 20:08:29 +0800

    fix fields

--- pygrounds_backend_new/urls.py
+++ pygrounds_backend_new/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView,)
+from users.views import UserProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('me/', UserProfileView.as_view(), name='user-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# users/views.py
+from rest_framework import permissions, generics
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class UserProfileView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
