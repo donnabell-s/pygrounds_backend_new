b2c850ea0d599316073f964f9a090a600037c273
donnabell-s <donnasembrano03@gmail.com>
2025-07-19 20:56:09 +0800

    working auth

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
-# project/urls.py
-from django.urls import path, include
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView,)
+# users/urls.py
+
+from django.urls import path
+from .views import (RegisterView, UserProfileView,)
 
 urlpatterns = [
-    path('api/user/', include('users.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('register/', RegisterView.as_view(), name='user-register'),
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
 ]
