f6e9e4c2867bb790c3a93e2595b8869ce7cb2327
donnabell-s <donnasembrano03@gmail.com>
2025-07-19 13:33:42 +0800

    auth migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ADMIN = 'admin'
+    LEARNER = 'learner'
+    
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (LEARNER, 'Learner'),
+    ]
+    
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=LEARNER)
+
+class LearnerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='learner_profile')
+
+class AdminProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='admin_profile')
