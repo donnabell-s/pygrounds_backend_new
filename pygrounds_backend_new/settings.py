d9491d20eef19506ab2a5b7938bb31989b9cc276
donnabell-s <donnasembrano03@gmail.com>
2025-07-19 13:33:42 +0800

    auth migrations

--- pygrounds_backend_new/settings.py
+++ pygrounds_backend_new/settings.py
@@ -50,6 +50,9 @@ DEEPSEEK_API_URL = config('DEEPSEEK_API_URL')
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.User'  # Replace with actual app name
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
