78db2ce0244e081d0743e3d475ab128a1d4a222e
kayemars <kayeduenlagahid@gmail.com>
2025-07-24 17:06:24 +0800

    Auto-tag difficulty on question creation

--- question_generation/models.py
+++ question_generation/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-
-from django.db import models
+from question_generation.utils.difficulty_predictor import predict_difficulty
 
 DIFFICULTY_LEVELS = (
     (1, "Easy"),
     (2, "Intermediate"),
     (3, "Hard"),
 )
+
+DIFFICULTY_MAPPING = {
+    "Easy": 1,
+    "Intermediate": 2,
+    "Hard": 3,
+}
 
 class Topic(models.Model):
     name = models.CharField(max_length=100)
@@ -27,6 +32,12 @@ class Question(models.Model):
     subtopic = models.ForeignKey(Subtopic, null=True, blank=True, on_delete=models.SET_NULL)
     difficulty = models.IntegerField(choices=DIFFICULTY_LEVELS, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.difficulty:
+            label = predict_difficulty(self.text)  # returns string like "Easy"
+            self.difficulty = DIFFICULTY_MAPPING.get(label, None)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.text[:50]
--- question_generation/urls.py
+++ question_generation/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import DocumentChunkClassificationView, DocumentChunkListView
+#from .views import DocumentChunkClassificationView, DocumentChunkListView
 
 urlpatterns = [
-    path('classify-document-chunks/', DocumentChunkClassificationView.as_view(), name='classify_document_chunks'),
-    path('document-chunks/', DocumentChunkListView.as_view(), name='document_chunks_list'),
-    path('list-classified-chunks/', DocumentChunkListView.as_view(), name='list_classified_chunks'),
+    #path('classify-document-chunks/', DocumentChunkClassificationView.as_view(), name='classify_document_chunks'),
+    #path('document-chunks/', DocumentChunkListView.as_view(), name='document_chunks_list'),
+    #path('list-classified-chunks/', DocumentChunkListView.as_view(), name='list_classified_chunks'),
 ]
 
