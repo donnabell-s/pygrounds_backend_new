38b6ed999856f8929ca7ee1c775d365f6ed5d9a5
kayemars <kayeduenlagahid@gmail.com>
2025-07-30 23:39:40 +0800

    added unique constraint and cleaned duplicate reading materials

--- reading/models.py
+++ reading/models.py
@@ -12,6 +12,9 @@ class ReadingMaterial(models.Model):
 
     class Meta:
         ordering = ['order_in_topic']
+        constraints = [
+            models.UniqueConstraint(fields=['topic', 'title'], name='unique_topic_title')
+        ]
 
     def __str__(self):
         return f"{self.topic} - {self.subtopic} - {self.title}"
